b549cefb957fc3811c8d79df5c0cf7793bdc567c
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2023-09-29 23:19:20 +0300

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallary = document.querySelector('.js-gallary');
+const markupGallary = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image js-gallary-target"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </li>
+`
+  )
+  .join('');
+
+gallary.innerHTML = markupGallary;
+
+gallary.addEventListener('click', onClick);
+
+function onClick(evt) {
+  if (!evt.target.classList.contains('js-gallary-target')) {
+    return;
+  }
+
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+        <img 
+        src="${evt.target.dataset.source}" 
+        width="800" 
+        height="600">
+    `);
+  instance.show();
+
+  document.addEventListener('keydown', evt => {
+    if (evt.code == 'Escape') {
+      instance.close();
+      console.log(evt.code);
+    }
+  });
+}
