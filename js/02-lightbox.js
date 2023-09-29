f8f8507fbb39b734ea345db257c260cdccff22f8
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2023-09-30 00:27:04 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallary = document.querySelector('.js-gallary');
+const markupGallary = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img 
+      class="gallery__image" 
+      src="${preview}" 
+      alt="${description}"
+      title="${description}"
+      />
+   </a>
+</li>
+`
+  )
+  .join('');
+
+gallary.innerHTML = markupGallary;
+
+gallary.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  
+  let lightbox  = new SimpleLightbox('.gallery a');
+
+}
+
+
